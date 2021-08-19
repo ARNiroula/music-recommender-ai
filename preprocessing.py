d36893e7dad9e50f2a15254a8447aa4a1f6e19b5
ARNiroula <40847587+ARNiroula@users.noreply.github.com>
2021-08-19 21:47:28 +0545

    preprocessed file separated

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,7 @@ warnings.filterwarnings('ignore')
 def ignore_warn(*args, **kwargs):
     pass
 
-warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
+warnings.warn = ignore_warn
 
 
 from IPython.display import display
@@ -22,20 +22,13 @@ from scipy.sparse.linalg import svds
 from scipy.sparse import csc_matrix
 from scipy.stats import skew, norm, probplot
 import seaborn as sns
-from item_similarity_recommender import ItemSimilarityRecommender 
-from popularity_recommender import PopularityRecommender
-
-
 
 sns.set(style="ticks", color_codes=True, font_scale=1.5)
 color = sns.color_palette()
 sns.set_style('darkgrid')
 
-track_metadata_df = pd.read_csv('./song_data.csv')
-count_play_df = pd.read_csv('./10000.txt', sep='\t', header=None, names=['user','song','play_count'])
-
-
-############# 1 #################
+track_metadata_df = pd.read_csv('./data/song_data.csv')
+count_play_df = pd.read_csv('./data/10000.txt', sep='\t', header=None, names=['user','song','play_count'])
 
 print('First see of track metadata:')
 print('Number of rows:', track_metadata_df.shape[0])
@@ -44,15 +37,17 @@ display(track_metadata_df.head())
 print('Note the problem with repeated track metadata. Let\'s see of counts play song by users:')
 display(count_play_df.shape, count_play_df.head())
 
-############# 1 #################
-
-############# 2 #################
-
 unique_track_metadata_df = track_metadata_df.groupby('song_id').max().reset_index()
 
 print('Number of rows after unique song Id treatment:', unique_track_metadata_df.shape[0])
 print('Number of unique songs:', len(unique_track_metadata_df.song_id.unique()))
 display(unique_track_metadata_df.head())
 
-############# 2 #################
+user_song_list_count = pd.merge(count_play_df, 
+                                unique_track_metadata_df, how='left', 
+                                left_on='song', 
+                                right_on='song_id')
+user_song_list_count.rename(columns={'play_count':'listen_count'},inplace=True)
+del(user_song_list_count['song_id'])
 
+user_song_list_count.to_csv('./data/preprocessed.csv')
